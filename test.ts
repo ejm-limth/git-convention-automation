95e969d4f09763d7b3e63ff5cd4a1af064662acf
ejm-limth <limth@ejmc.kr>
2024-04-23 11:20:10 +0900

    🎨 Style: emoji
    WRGDEV-1234

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 console.log("Feat ");
 console.log("Fix ");
 console.log("Docs ");
-console.log("Style");
+console.log("Style ");
 console.log("Refactor");
 console.log("Test");
 console.log("Chore");
