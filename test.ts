fe4160d73a8e521c18c9cac03757a294a3dedcca
ejm-limth <limth@ejmc.kr>
2024-04-23 11:20:36 +0900

    ☔️ Test: emoji
    WRGDEV-1234

--- test.ts
+++ test.ts
@@ -3,7 +3,7 @@ console.log("Fix ");
 console.log("Docs ");
 console.log("Style ");
 console.log("Refactor ");
-console.log("Test");
+console.log("Test ");
 console.log("Chore");
 console.log("Design");
 console.log("Add");
